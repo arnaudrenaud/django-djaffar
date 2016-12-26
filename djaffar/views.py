0ca4096e9c0a158508d9903dfe86fff45e7f895a
Arnaud Renaud <arnaud.renaud@gmail.com>
2016-12-26 22:20:02 +0100

    Complete tests, refactor log POST view

--- djaffar/views.py
+++ djaffar/views.py
@@ -13,18 +13,16 @@ from django.contrib.sessions.models import Session
 from .models import Activity, SessionInfo
 
 
+
 class LogActivity(views.APIView):
 
-    def post(self, request):
+    def _session(self, request):
         if not request.session.exists(request.session.session_key):
             request.session.create()
             session = Session.objects.get(pk=request.session.session_key)
             SessionInfo.objects.get_or_create(
                 session=session,
                 user_agent=request.META.get('HTTP_USER_AGENT', ''))
-        user = request.user
-        if user.is_anonymous():
-            user = None
         try:
             session_id = request.session.session_key
             try:
@@ -33,12 +31,14 @@ class LogActivity(views.APIView):
                 session = None
         except KeyError:
             session = None
-        path = (request.data.get('path') or
-                urlparse(request.META.get('HTTP_REFERER', '')).path)
-        if not path:
-            return Response(
-                'You must provide a path',
-                status=status.HTTP_400_BAD_REQUEST)
+        return session
+
+    def post(self, request):
+        user = request.user
+        if user.is_anonymous():
+            user = None
+        session = self._session(request)
+        ip_address = request.META.get('REMOTE_ADDR', '')
         dt = request.data.get('date')
         if not dt:
             return Response(
@@ -50,10 +50,13 @@ class LogActivity(views.APIView):
                 return Response(
                     'You must provide a valid date',
                     status=status.HTTP_400_BAD_REQUEST)
-        referer = request.data.get('referer')
-        if referer == 'NOT_FIRST_PAGE' or not referer:
-            referer = ''
-        ip_address = request.META.get('REMOTE_ADDR', '')
+        path = (request.data.get('path') or
+                urlparse(request.META.get('HTTP_REFERER', '')).path)
+        if not path:
+            return Response(
+                'You must provide a path',
+                status=status.HTTP_400_BAD_REQUEST)
+        referer = request.data.get('referer', '')
         Activity.objects.create(
             user=user,
             session=session,
